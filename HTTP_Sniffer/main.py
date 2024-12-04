cf3e3beab6676b0a9eed0481a766e9d92f85af2d
AyakaiKami <stefancristea27@gmail.com>
2024-12-05 00:21:44 +0200

    Started the gui

--- HTTP_Sniffer/main.py
+++ HTTP_Sniffer/main.py
@@ -8,6 +8,8 @@ import os
 import argparse
 import threading
 import time
+from tkinter import * 
+import psutil
 
 
 from packet import Ethernet_Frame as Ethernet_Frame
@@ -30,12 +32,22 @@ def sniff(interface):
     except KeyboardInterrupt:
         return
 
-if __name__=='__main__':
-    if os.getuid()!=0:
-        print("This script needs root privilleges to run!")
-        sys.exit(1)
 
-    print("Starting Sniffer!")
+if os.getuid()!=0:
+    print("This script needs root privilleges to run!")
+    sys.exit(1)
+
+interfaces_list=list(psutil.net_if_addrs().keys())
     
-    sniff('wlan0')
-    print("Sniffer has been stopped")
+#sniff('wlan0')
+root = Tk()
+root.geometry(f"{1200}x{800}+{int(root.winfo_screenwidth()/2)-600}+{int(root.winfo_screenheight()/2)-400}")
+root.configure(bg="black")
+root.title('HTTP Sniffer') # Change name
+
+
+label_select_interface=Label(root,text="Select network interface:",bg="black",fg="white")
+label_select_interface.pack(pady=10,padx=10)
+
+root.mainloop()
+
