5242614283aaa3681cab611d4d7b07c0b7b02606
AyakaiKami <stefancristea27@gmail.com>
2024-12-04 15:14:49 +0200

    Closer

--- HTTP_Sniffer/request.py
+++ HTTP_Sniffer/request.py
@@ -5,19 +5,20 @@ class Requests:
     def addPacket(packet:Ethernet_Frame):
         if packet.is_tcp_packet:
             key=f"{packet.source_ip}:{packet.source_port}->{packet.destination_ip}:{packet.destination_port}"
-            if Requests.requests_list.get(key) == None and "HTTP" in packet.payload:
-                Requests.requests_list[key]={'sequence_number':packet.sequence_number,'payload':packet.payload}
+            if Requests.requests_list.get(key) == None:
+                if "HTTP" in packet.payload.split('\n')[0]:
+                    Requests.requests_list[key]={'sequence_number':packet.sequence_number,'payload':packet.payload}
             else:
                 if Requests.requests_list.get(key) != None:
                     if Requests.requests_list[key]['sequence_number']<packet.sequence_number:
                         Requests.requests_list[key]['sequence_number']=packet.sequence_number
                         Requests.requests_list[key]['payload']+=packet.payload
 
-                        if "\r\n" in Requests.requests_list[key]['payload']:
-                            print(f"Packet :{packet.source_ip}:{packet.source_port}->{packet.destination_ip}:{packet.destination_port}")
-                            print("----------Start payload:")
-                            print(Requests.requests_list[key]['payload'])
-                            print("----------End Payload\n")
-                            Requests.requests_list[key] = None
+                    if "\r\n" in Requests.requests_list[key]['payload']:
+                        print(f"Packet :{packet.source_ip}:{packet.source_port}->{packet.destination_ip}:{packet.destination_port}")
+                        print("----------Start payload:")
+                        print(Requests.requests_list[key]['payload'])
+                        print("----------End Payload\n")
+                        Requests.requests_list[key] = None
             pass
     
