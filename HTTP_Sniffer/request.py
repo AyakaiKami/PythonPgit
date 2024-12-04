f4c55a3b43bcc2047c621aac45698fc88dd895d3
AyakaiKami <stefancristea27@gmail.com>
2024-12-04 15:30:27 +0200

    even closer

--- HTTP_Sniffer/request.py
+++ HTTP_Sniffer/request.py
@@ -8,13 +8,20 @@ class Requests:
             if Requests.requests_list.get(key) == None:
                 if "HTTP" in packet.payload.split('\n')[0]:
                     Requests.requests_list[key]={'sequence_number':packet.sequence_number,'payload':packet.payload}
-            else:
-                if Requests.requests_list.get(key) != None:
-                    if Requests.requests_list[key]['sequence_number']<packet.sequence_number:
-                        Requests.requests_list[key]['sequence_number']=packet.sequence_number
-                        Requests.requests_list[key]['payload']+=packet.payload
+                    
+                    if Requests.requests_list[key]['payload'].endswith(("\r\n","</html>")):
+                        print(f"Packet :{packet.source_ip}:{packet.source_port}->{packet.destination_ip}:{packet.destination_port}")
+                        print("----------Start payload:")
+                        print(Requests.requests_list[key]['payload'])
+                        print("----------End Payload\n")
+                        Requests.requests_list[key] = None
 
-                    if "\r\n" in Requests.requests_list[key]['payload']:
+            else:
+                if Requests.requests_list[key]['sequence_number']<packet.sequence_number:
+                    Requests.requests_list[key]['sequence_number']=packet.sequence_number
+                    Requests.requests_list[key]['payload']+=packet.payload
+
+                    if Requests.requests_list[key]['payload'].endswith(("\r\n","</html>")) :
                         print(f"Packet :{packet.source_ip}:{packet.source_port}->{packet.destination_ip}:{packet.destination_port}")
                         print("----------Start payload:")
                         print(Requests.requests_list[key]['payload'])
